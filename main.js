5b87b1b3caab8d6a66915fcc3bd64ef28a348182
chaeeunj <official2930@naver.com>
2023-06-23 03:45:36 +0900

    상품 데이터 연결

--- main.js
+++ main.js
@@ -1,29 +1,18 @@
 import { getProductSection } from "./module/productSection.js";
 
-const productSection = getProductSection('인기 상품', [
-{
-  "id": 4,
-  "imgSrc": "/public/assets/삼겹살.jpg",
-  "name": "구이용 삼겹살 600g (냉장)",
-  "discountPercent": 5,
-  "price": 14820,
-  "originalPrice": 15600
-},
-{
-  "id": 5,
-  "imgSrc": "/public/assets/머핀.jpg",
-  "name": "[홍대 W마켓] (2입)",
-  "discountPercent": 20,
-  "price": 4800,
-  "originalPrice": 6000
-},
-{
-  "id": 6,
-  "imgSrc": "/public/assets/원두.jpg",
-  "name": "[카페 W] 디카페인 원두 1kg",
-  "discountPercent": 15,
-  "price": 7140,
-  "originalPrice": 8400
-}]);
+try {
+  const response = await fetch('public/mock/sectionListData.json');
+  const data = await response.json();
+  const sectionInfoList = data.sectionInfoList;
 
-document.body.appendChild(productSection);
+  sectionInfoList.forEach((sectionInfo) => {
+    console.log(sectionInfo);
+    const { sectionTitle, productList } = sectionInfo;
+    const productSectionDOM = getProductSection(sectionTitle, productList);
+    document.body.appendChild(productSectionDOM);
+  })
+} catch(error) {
+  console.log(error);
+}
+
+// document.body.appendChild(productSection);
