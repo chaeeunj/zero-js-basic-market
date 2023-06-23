d373c97247df8f9c916e79952c0fa8ca69ad5043
chaeeunj <official2930@naver.com>
2023-06-23 13:18:50 +0900

    장바구니 페이지 목록

--- module/cartToggleButton.js
+++ module/cartToggleButton.js
@@ -1,16 +1,19 @@
 import { makeDOMwithProperties } from "../utils/dom.js";
 import { CART_COOKIE_KEY } from "../constants/cart.js";
 
+
+export const getCartInfo = () => JSON.parse(localStorage.getItem(CART_COOKIE_KEY)) || [];
+
 const isInCart = ({ id }) => {
   // 현재 해당 상품이 장바구니 안에 있는지를 판단하여 결과를 반환
-  const originalCartInfo = JSON.parse(localStorage.getItem(CART_COOKIE_KEY)) || [];
+  const originalCartInfo = getCartInfo();
   // 바로 해당 값을 리턴함
   return !!originalCartInfo.find((cartInfo) => cartInfo.id === id);
 }
 
 const addCartInfo = (productInfo) => {
   // 장바구니에 해당 물품의 정보를 저장
-  const originalCartInfo = JSON.parse(localStorage.getItem(CART_COOKIE_KEY)) || [];
+  const originalCartInfo = getCartInfo();
 
  if (originalCartInfo.findIndex((cartInfo) => cartInfo.id === productInfo.id) !== -1) return;
 
@@ -22,7 +25,7 @@ const addCartInfo = (productInfo) => {
 
 const removeCartInfo = ({ id }) => {
   // 장바구니에서 해당 물품의 정보를 삭제
-  const originalCartInfo = JSON.parse(localStorage.getItem(CART_COOKIE_KEY)) || [];
+  const originalCartInfo = getCartInfo();
   const newCartInfo = originalCartInfo.filter((cartInfo) => cartInfo.id !== id); // false true true
 
   localStorage.setItem(CART_COOKIE_KEY, JSON.stringify(newCartInfo));
